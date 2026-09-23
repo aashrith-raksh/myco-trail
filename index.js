8d33a193a2ba7f71122cf2c80ee9d67f42bdb6d4
Aashrith Raksh <aashrith.raksh@gmail.com>
2026-09-23 15:23:08 +0530

    add card-filtering feature

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const navToggle = document.querySelector('[aria-controls="site-navigation"]');
 navToggle.addEventListener("click", () => {
   navigationOpen = navToggle.getAttribute("aria-expanded");
-  console.log(navigationOpen)
 
   if (navigationOpen === "true") {
     navToggle.setAttribute("aria-expanded", "false");
@@ -9,3 +8,42 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", "true");
   }
 });
+
+const seasonFilter = document.querySelector("#season-filter");
+const edibleFilter = document.querySelector("#edible-filter");
+const mushroomCards = Array.from(
+  document.querySelectorAll(".know-your-mushrooms-section .card"),
+);
+
+let currentFilters = {
+  edible: "all",
+  season: "all",
+};
+
+function filterCards(currentFilters) {
+  const edibleFilter = currentFilters.edible;
+  const seasonFilter = currentFilters.season;
+
+  mushroomCards.forEach(card => {
+    const edibleTag = card.querySelector("[data-edible]").dataset.edible;
+    const seasonTag = card.querySelector("[data-season]").dataset.season;
+
+    const edibleMatch = (edibleFilter === "all") || (edibleTag === (edibleFilter === "edible"? "true": "false"))
+    const seasonMatch = (seasonFilter === "all") || (seasonFilter === seasonTag)
+
+    card.hidden = !(edibleMatch && seasonMatch)
+  })
+}
+
+function handleEdibleFilterChange(event) {
+  currentFilters.edible = event.target.value;
+  filterCards(currentFilters);
+}
+
+function handleSeasonFilterChange(event) {
+  currentFilters.season = event.target.value;
+  filterCards(currentFilters);
+}
+
+seasonFilter.addEventListener("change", handleSeasonFilterChange);
+edibleFilter.addEventListener("change", handleEdibleFilterChange);
